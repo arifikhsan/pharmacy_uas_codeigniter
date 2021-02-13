f5030ffb3f1d0fffc71119fdfa730c2ce93d0469
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-02-13 22:47:15 +0700

    drug seeder

---
+++ app/Database/Seeds/DrugSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class DrugSeeder extends Seeder
+{
+	public function run()
+	{
+		$drug = model('Drug');
+
+    for ($i = 0; $i < 5; $i++) {
+      $drug->insert([
+        'supplier_id' => static::faker()->numberBetween(1, 3),
+        'name' => static::faker()->name,
+        'price' => static::faker()->numberBetween(1, 10) * 1000,
+        'quantity' => static::faker()->numberBetween(1, 10) * 100,
+      ]);
+    }
+	}
+}
